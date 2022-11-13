463f2afd3334bb0e6a53b60a734a16e62b15626d
hktrollgames <hktrollgames@gmail.com>
2022-11-13 15:41:34 +0900

    효과 실습

---
+++ Assets/UIEffectControll.cs
@@ -0,0 +1,29 @@
+﻿using Coffee.UIEffects;
+using NaughtyAttributes;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIEffectControll : MonoBehaviour
+{
+
+    [Button]
+    public void SetActiveEffect()
+    {
+        UIEffect uIEffect = GetComponent<UIEffect>();
+        uIEffect.effectFactor = 0;
+        uIEffect.blurFactor = 0;
+    }
+    [Button("비활성화")]
+    public void SetDeactiveEffect()
+    {
+        UIEffect uIEffect = GetComponent<UIEffect>();
+        uIEffect.effectFactor = 1;
+        uIEffect.blurFactor = 1;
+    }
+    [Button("비활성화 애니메이션")]
+    public void SetDeactiveEffectAnimation()
+    {
+        GetComponent<Animator>().Play("Deactive");
+    }
+}
