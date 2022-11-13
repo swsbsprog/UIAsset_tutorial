2ca2fb223b60ab61d8b0db1d3293548e46b92f60
hktrollgames <hktrollgames@gmail.com>
2022-11-13 17:40:38 +0900

    인벤토리 실습시작

---
+++ Assets/DropMe.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class DropMe : MonoBehaviour, IDragHandler
+{
+    public void OnDrag(PointerEventData eventData)
+    {
+        transform.position = eventData.position;
+    }
+}
