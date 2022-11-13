ac7e59285e1b144dcd2f359c76ff3a64308b9c92
hktrollgames <hktrollgames@gmail.com>
2022-11-13 14:00:53 +0900

    초기 커밋.

---
+++ Assets/NaughtyAttributes/Scripts/Test/ButtonTest.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using UnityEngine;
+
+namespace NaughtyAttributes.Test
+{
+    public class ButtonTest : MonoBehaviour
+    {
+        public int myInt;
+
+        [Button(enabledMode: EButtonEnableMode.Always)]
+        private void IncrementMyInt()
+        {
+            myInt++;
+        }
+
+        [Button("Decrement My Int", EButtonEnableMode.Editor)]
+        private void DecrementMyInt()
+        {
+            myInt--;
+        }
+
+        [Button(enabledMode: EButtonEnableMode.Playmode)]
+        private void LogMyInt(string prefix = "MyInt = ")
+        {
+            Debug.Log(prefix + myInt);
+        } 
+
+        public string MyFnParam;
+        [Button(enabledMode: EButtonEnableMode.Always)]
+        private void MyFn()
+        {   
+            Debug.Log(MyFnParam);
+        }
+
+        [Button("StartCoroutine")]
+        private IEnumerator IncrementMyIntCoroutine()
+        {
+            int seconds = 5;
+            for (int i = 0; i < seconds; i++)
+            {
+                myInt++;
+                yield return new WaitForSeconds(1.0f);
+            }
+        }
+    }
+}
